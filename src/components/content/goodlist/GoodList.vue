66a5b2e6ed22659a56a16ab11d7bd5f3ba94af0e
lixinjian <2128822691@qq.com>
2022-02-02 19:57:48 +0800

    首页数据展示

---
+++ src/components/content/goodlist/GoodList.vue
@@ -0,0 +1,36 @@
+<!--  -->
+<template>
+  <div class="goods">
+    <good-list-item v-for="item in goods" :goodInfor="item"></good-list-item>
+  </div>
+</template>
+
+<script>
+import GoodListItem from './GoodListItem.vue'
+export default {
+
+  name: "GoodList",
+  data() {
+    return {};
+  },
+  props: {
+    goods: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  },
+  components: {
+    GoodListItem
+  }
+};
+</script>
+<style scoped>
+.goods {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  padding: 2px;
+}
+</style>
